549b386ffd1fad9981d46ff04deafe0a35ed0f98
Uday Shastrakar <uday.shastrakar@gmail.com>
2025-07-26 19:45:13 +0530

    config file done

---
+++ src/main/java/com/bank/gateway/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.bank.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+    @GetMapping("/public")
+    public String publicApi(){
+        return "this is public endpoint";
+    }
+    @GetMapping("/private")
+    public String privateApi(){
+        return "this is private endpoint";
+    }
+}
