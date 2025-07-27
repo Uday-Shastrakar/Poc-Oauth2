a7f2facbf8d8a2dee8e60a05d4cab42d506fe8fd
Uday Shastrakar <uday.shastrakar@gmail.com>
2025-07-27 20:14:51 +0530

    keycloack config added

---
+++ src/main/java/com/bank/gateway/config/KeyclockRoleConverter.java
@@ -0,0 +1,41 @@
+package com.bank.gateway.config;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeyclockRoleConverter implements Converter<Jwt , Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String,Object> realmAccess =(Map<String, Object>) jwt.getClaim("realm_access");
+        if(realmAccess == null || realmAccess.isEmpty()){
+            return new ArrayList<>();
+        }
+
+        Collection<GrantedAuthority> returnValue = ((List<String>) realmAccess.get("roles"))
+                .stream().map(roleName -> "ROLE_"+roleName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return  returnValue;
+    }
+
+    @Override
+    public JavaType getInputType(TypeFactory typeFactory) {
+        return null;
+    }
+
+    @Override
+    public JavaType getOutputType(TypeFactory typeFactory) {
+        return null;
+    }
+
+}
--- src/main/java/com/bank/gateway/config/SecurityConfig.java
+++ src/main/java/com/bank/gateway/config/SecurityConfig.java
@@ -2,10 +2,20 @@ package com.bank.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,12 +25,23 @@ public class SecurityConfig {
     public SecurityWebFilterChain  securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/api/public/**").permitAll()
-                        .anyExchange().authenticated()
+                        .pathMatchers("/api/public/**").hasRole("Normal")
+                        .pathMatchers("/api/private/**").hasRole("admin")
+//                        .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return http.build();
     }
+
+        private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+            JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+            jwtConverter.setJwtGrantedAuthoritiesConverter((Converter<Jwt, Collection<GrantedAuthority>>) new KeyclockRoleConverter());
+
+            return new ReactiveJwtAuthenticationConverterAdapter(jwtConverter);
+        }
+
+
 }
